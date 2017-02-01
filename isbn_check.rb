c081ffc89a7813bc7a1e91a29bb1ebe86106a962
jverbosky <jverbosk@gmail.com>
2017-02-01 16:24:04 -0500

    wrote method to strip last character from isbn number

--- isbn_check.rb
+++ isbn_check.rb
@@ -20,7 +20,15 @@ def output_raw_number(isbn)
 
 end
 
+def all_but_last(isbn)
+
+	trimmed = isbn[0..-2]
+
+	return trimmed
+
+end
 
 puts output_raw_number("0-321-14653-0")
 puts output_raw_number("877 1 95 869x")
 puts output_raw_number("0471958697")
+puts all_but_last("7421394761")
