31abfb99c7ebbcf1a2d279771451d6c7f238fa61
jverbosky <jverbosk@gmail.com>
2017-02-01 15:58:21 -0500

    wrote third test

---
+++ isbn_check.rb
@@ -0,0 +1,25 @@
+# Program to determine if ISBN number is valid or not (true/false)
+
+# Method to convert hyphenated or space-delimited ISBN number to raw number
+def output_raw_number(isbn)
+
+	raw_number = []
+	hyphen = "-"
+	space = " "
+	isbn_array = isbn.split("")
+
+	isbn_array.each do |character|
+		if character != hyphen
+			if character != space
+				raw_number.push(character)
+			end
+		end
+	end
+
+	return raw_number.join("")
+
+end
+
+
+puts output_raw_number("0-321-14653-0")
+puts output_raw_number("877 1 95 869x")
--- isbn_check_test.rb
+++ isbn_check_test.rb
@@ -5,15 +5,21 @@ require_relative "isbn_check.rb"
 class TestISBN < Minitest::Test
 
 	def test_1_output_raw_number_source_with_hyphens
-		isbn = "978-0-13-149505-0"
+		isbn = "0-321-14653-0"
 		results = output_raw_number(isbn)
-		assert_equal(9780131495050, results)
+		assert_equal("0321146530", results)
 	end
 
 	def test_2_output_raw_number_source_with_spaces
-		isbn = "978 0 13 149505 0"
+		isbn = "877 1 95 869x"
 		results = output_raw_number(isbn)
-		assert_equal(9780131495050, results)
+		assert_equal("877195869x", results)
+	end
+
+	def test_3_no_change_in_ouput_if_raw_isbn_number
+		isbn = "0471958697"
+		results = output_raw_number(isbn)
+		assert_equal("0471958697", results)
 	end
 
 end
