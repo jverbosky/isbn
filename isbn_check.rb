4b6b8347634de0a1569655819fe2cfa8fcd970b7
jverbosk <jverbosk@gmail.com>
2017-02-02 19:05:22 -0500

    refactored function and test done - made valid_characters? method more robus, passing all tests now

--- isbn_check.rb
+++ isbn_check.rb
@@ -80,19 +80,30 @@ end
 
 # Method to screen out ISBN numbers with invalid characters (before passing to other methods)
 def valid_characters?(isbn)
-    isbn_array = isbn.split("")  # create an array from characters in isbn number
-    valid_characters = "0123456789 -xX".split("")  # create an array of valid isbn number characters
-    invalid_character_count = 0  # counter for invalid characters
-    isbn_array.each do |character|  # iterate through array to check each character in isbn number
-        unless valid_characters.include?(character)  # if the current character is not in the valid characters array
-            invalid_character_count += 1  # then increment the invalid character counter
-        end
+  isbn_array = isbn.split("")  # create an array from characters in isbn number
+  first_nine = isbn_array[0..-2]  # first nine elements of isbn_array to check for x/X
+  valid_characters = "0123456789 -xX".split("")  # create an array of valid isbn number characters
+  invalid_character_count = 0  # counter for invalid characters
+  if isbn.length < 10  # if the number is less than 10, it's not a valid isbn number
+    return false  # so return false
+  end
+  isbn_array.each do |character|  # iterate through array to check each character in isbn number
+    unless valid_characters.include?(character)  # if the current character is not in the valid characters array
+      invalid_character_count += 1  # then increment the invalid character counter
+      break  # since one invalid character is enough, break if we find one to save cycles
     end
-    if invalid_character_count > 0  # if there are any invalid characters
-        return false  # then return false
-    else
-        valid_checksum?(isbn)  # otherwise, pass the isbn number to the valid_checksum? method
+  end
+  first_nine.each do |character|  # iterate through array to check each character in the first nine
+    if character == "x" || character == "X"  # if the current character is an x/X
+      invalid_character_count += 1  # then increment the invalid character counter
+      break  # since one invalid character is enough, break if we find one to save cycles
     end
+  end
+  if invalid_character_count > 0  # if there are any invalid characters
+    return false  # then return false
+  else
+    valid_checksum?(isbn)  # otherwise, pass the isbn number to the valid_checksum? method
+  end
 end
 
 # Sandbox testing:
